a3bea9404937378ce34f8b3486b4c99b9d078e04
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-06 19:21:31 -0400

    Update addCC.sql

--- addCC.sql
+++ addCC.sql
@@ -8,3 +8,5 @@ INSERT INTO CRCARD (ccType, ccNum, ExpDate, crLimit)
 VALUES ('M', '1000200030004000', TO_DATE('12/01/2025', 'MM/DD/YYYY'), 3000.00);
 
 COMMIT;
+
+-- Had to add COLUMN ccType FORMAT A6 to get format right.
