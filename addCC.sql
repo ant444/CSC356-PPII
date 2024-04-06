8f75182a747fb21ed6385dfc68630e07de6e17a9
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-06 19:09:00 -0400

    Create addCC.sql

---
+++ addCC.sql
@@ -0,0 +1,10 @@
+-- Filename: addCC.sql
+
+-- Inserting credit cards
+INSERT INTO CRCARD (ccType, ccNum, ExpDate, crLimit)
+VALUES ('V', '4000100020003000', TO_DATE('09/01/2024', 'MM/DD/YYYY'), 2500.00);
+
+INSERT INTO CRCARD (ccType, ccNum, ExpDate, crLimit)
+VALUES ('M', '1000200030004000', TO_DATE('12/01/2025', 'MM/DD/YYYY'), 3000.00);
+
+COMMIT;
