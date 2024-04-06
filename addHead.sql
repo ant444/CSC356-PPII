a2c733ddcc2b93ca14247d9548b2f9ecb72d5ef2
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-06 16:00:54 -0400

    Update addHead.sql

--- addHead.sql
+++ addHead.sql
@@ -1,1 +1,30 @@
 -- Filename: addHead.sql
+
+-- addHead.sql
+
+-- Inserting data into the HEAD table
+INSERT INTO HEAD (Code, Name, HType, OpBal, HDate)
+VALUES (1000, 'Cash', 'A', 2527.00, TO_DATE('01/01/21', 'MM/DD/YY'));
+
+INSERT INTO HEAD (Code, Name, HType, OpBal, HDate)
+VALUES (1010, 'PNC Account', 'A', 3000.00, TO_DATE('01/01/21', 'MM/DD/YY'));
+
+INSERT INTO HEAD (Code, Name, HType, OpBal, HDate)
+VALUES (1020, 'Wells Fargo Account', 'A', 0.00, TO_DATE('01/07/21', 'MM/DD/YY'));
+
+INSERT INTO HEAD (Code, Name, HType, OpBal, HDate)
+VALUES (1030, 'Chase Account', 'L', 0.00, TO_DATE('01/07/21', 'MM/DD/YY'));
+
+INSERT INTO HEAD (Code, Name, HType, OpBal, HDate)
+VALUES (2010, 'Grocery', 'E', 0.00, TO_DATE('01/01/21', 'MM/DD/YY'));
+
+INSERT INTO HEAD (Code, Name, HType, OpBal, HDate)
+VALUES (3010, 'Utilities', 'E', 0.00, TO_DATE('01/01/21', 'MM/DD/YY'));
+
+INSERT INTO HEAD (Code, Name, HType, OpBal, HDate)
+VALUES (4010, 'Entertainment', 'E', 0.00, TO_DATE('01/07/21', 'MM/DD/YY'));
+
+INSERT INTO HEAD (Code, Name, HType, OpBal, HDate)
+VALUES (9010, 'Misc. Expense', 'E', 0.00, TO_DATE('01/01/21', 'MM/DD/YY'));
+
+COMMIT;
