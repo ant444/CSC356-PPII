8c814a70391db27230a09759844a3678b644993c
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-06 21:11:39 -0400

    Create created.sql

---
+++ created.sql
@@ -0,0 +1,15 @@
+-- createdb.sql
+
+-- Run deldb.sql to delete the existing database
+@deldb.sql
+
+-- Run the scripts to recreate the database and populate it with data
+-- Note: Adjust the paths and names of the scripts based on your file system
+@createSchema.sql
+@addSub.sql
+@addCC.sql
+@addStore.sql
+@addExpense.sql
+
+-- Exit SQL*Plus
+--EXIT;
