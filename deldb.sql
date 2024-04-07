860fccb355fcff89b778d011ed902e5b73f91d63
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-06 21:09:26 -0400

    Create deldb.sql

---
+++ deldb.sql
@@ -0,0 +1,23 @@
+-- deldb.sql
+
+-- Disable foreign key constraints
+SET CONSTRAINTS ALL DEFERRED;
+
+-- Drop tables in reverse order to maintain referential integrity
+DROP TABLE ExpByCC;
+DROP TABLE ExpDet;
+DROP TABLE ExpMast;
+DROP TABLE CRCARD;
+DROP TABLE SUBHEAD;
+DROP TABLE HEAD;
+DROP TABLE STORE;
+
+-- Drop any remaining constraints
+-- Note: You may need to adjust this section based on your specific database schema
+-- ALTER TABLE table_name DROP CONSTRAINT constraint_name;
+
+-- Commit the changes
+COMMIT;
+
+-- Exit SQL*Plus
+EXIT;
