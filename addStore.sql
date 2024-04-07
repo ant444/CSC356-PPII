c341453084861553546d165ad9a08852780c9c18
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-06 21:22:09 -0400

    Create addStore.sql

---
+++ addStore.sql
@@ -0,0 +1,11 @@
+-- addStore.sql
+ 
+-- Inserting stores
+INSERT INTO STORE (Name, Code) VALUES ('Walmart', 'WM');
+INSERT INTO STORE (Name, Code) VALUES ('Giant', 'GT');
+INSERT INTO STORE (Name, Code) VALUES ('Turkey Hill', 'TH');
+INSERT INTO STORE (Name, Code) VALUES ('MetEd', 'ME');
+INSERT INTO STORE (Name, Code) VALUES ('Misc.', 'Mx');
+
+COMMIT;
+
