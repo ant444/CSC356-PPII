78aa63ed7ab99e07cedd9b69c6e332167cee1dce
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-28 17:14:06 -0400

    Update getexpcc.sql

--- getexpcc.sql
+++ getexpcc.sql
@@ -4,7 +4,7 @@ ACCEPT start_date PROMPT 'Enter Start Date (YYYY-MM-DD): '
 ACCEPT end_date PROMPT 'Enter End Date (YYYY-MM-DD): '
 ACCEPT cc_number PROMPT 'Enter Credit Card Number: '
 
-SELECT ExpNumber, ExpDate, VendorName, Amount
+SELECT ExpNumber, ExpDate, StoreCode, Amount
 FROM ExpMast
 WHERE ExpDate BETWEEN TO_DATE('&start_date', 'YYYY-MM-DD') AND TO_DATE('&end_date', 'YYYY-MM-DD')
 AND ExpNumber IN (
