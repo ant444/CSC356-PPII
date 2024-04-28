bfda920f614aa3e00b4261ce4430a3d6a08a297c
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-28 17:17:46 -0400

    Update getexpcc.sql

--- getexpcc.sql
+++ getexpcc.sql
@@ -4,7 +4,7 @@ ACCEPT start_date PROMPT 'Enter Start Date (YYYY-MM-DD): '
 ACCEPT end_date PROMPT 'Enter End Date (YYYY-MM-DD): '
 ACCEPT cc_number PROMPT 'Enter Credit Card Number: '
 
-SELECT ExpNum, ExpDate, StoreCode, Amount
+SELECT ExpNum, ExpDate, StoreCode, Amt
 FROM ExpMast
 WHERE ExpDate BETWEEN TO_DATE('&start_date', 'YYYY-MM-DD') AND TO_DATE('&end_date', 'YYYY-MM-DD')
 AND ExpNum IN (
@@ -13,7 +13,7 @@ AND ExpNum IN (
     WHERE ccNum = '&cc_number'
 );
 
-SELECT SUM(Amount) AS TotalAmount
+SELECT SUM(Amt) AS TotalAmount
 FROM ExpMast
 WHERE ExpDate BETWEEN TO_DATE('&start_date', 'YYYY-MM-DD') AND TO_DATE('&end_date', 'YYYY-MM-DD')
 AND ExpNum IN (
