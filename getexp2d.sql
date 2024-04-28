ee319ff443c60b5369dfb80666a6d8e16322544d
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-28 16:56:26 -0400

    Update getexp2d.sql

--- getexp2d.sql
+++ getexp2d.sql
@@ -2,7 +2,7 @@
 
 ACCEPT given_date PROMPT 'Enter Date (YYYY-MM-DD): '
 
-SELECT em.ExpNumber, em.ExpDate, em.VendorName, em.Amount, ed.ItemName, ed.Quantity, ed.UnitPrice
+SELECT em.ExpNumber, em.ExpDate, em.StoreCode, em.CashAmt, em.Descr
 FROM ExpMast em
 JOIN ExpDet ed ON em.ExpNumber = ed.ExpNumber
 WHERE em.ExpDate = TO_DATE('&given_date', 'YYYY-MM-DD');
