8f4c4d865314e5be8c5f38814cb2c86eb3c8d749
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-29 09:20:45 -0400

    Update getexpcc.sql

--- getexpcc.sql
+++ getexpcc.sql
@@ -1,7 +1,7 @@
  -- File: getexpcc.sql
  
- ACCEPT start_date PROMPT 'Enter Start Date (YYYY-MM-DD): '
- ACCEPT end_date PROMPT 'Enter End Date (YYYY-MM-DD): '
+ ACCEPT start_date PROMPT 'Enter Start Date (DD-MON-YY): '
+ ACCEPT end_date PROMPT 'Enter End Date (DD-MON-YY): '
  ACCEPT cc_number PROMPT 'Enter Credit Card Number: '
      
  -- Display all expenses (without details) incurred between two user-given dates f>
@@ -9,10 +9,10 @@
  FROM ExpByCC
  JOIN ExpMast ON ExpByCC.ExpNum = ExpMast.ExpNum
  WHERE ExpByCC.ccNum = '&cc_number'
-   AND ExpMast.ExpDate BETWEEN TO_DATE('&start_date', 'YYYY-MM-DD') AND TO_DATE('&end_date', 'YYYY-MM-DD')
+   AND ExpMast.ExpDate BETWEEN '&start_date' AND '&end_date'
  UNION ALL
  SELECT NULL, NULL, NULL, SUM(Amt) AS TotalAmount
  FROM ExpByCC
  JOIN ExpMast ON ExpByCC.ExpNum = ExpMast.ExpNum
  WHERE ExpByCC.ccNum = '&cc_number'
-   AND ExpMast.ExpDate BETWEEN TO_DATE('&start_date', 'YYYY-MM-DD') AND TO_DATE('&end_date', 'YYYY-MM-DD');
+   AND ExpMast.ExpDate BETWEEN '&start_date' AND '&end_date' ;
