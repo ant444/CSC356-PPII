05e4b5f2ea84bb5a4612152cbccca9ff3dd18624
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-29 09:14:40 -0400

    Update addExpMast.sql

--- addExpMast.sql
+++ addExpMast.sql
@@ -2,10 +2,10 @@
 
   -- File: addExpMast.sql
   
-  ACCEPT ExpDate PROMPT 'Enter Expense Date (YYYY-MM-DD): '
+  ACCEPT ExpDate PROMPT 'Enter Expense Date (DD-MON-YY): '
   ACCEPT CashAmt PROMPT 'Enter	Cash Amount Spent: $'
   ACCEPT StoreCode PROMPT 'Enter the Store Code: '
   
   INSERT INTO ExpMast (ExpDate, CashAmt, StoreCode)
-  VALUES (TO_DATE('&ExpDate', 'YYYY-MM-DD'), '&CashAmt', '&StoreCode')	;
+  VALUES ('&ExpDate', '&CashAmt', '&StoreCode')	;
   
