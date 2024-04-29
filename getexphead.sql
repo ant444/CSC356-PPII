9e016da65a4f5eb30d9a5a5369be1cb26dcc738d
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-29 09:21:49 -0400

    Update getexphead.sql

--- getexphead.sql
+++ getexphead.sql
@@ -1,10 +1,10 @@
 -- File: getexphead.sql
 
 ACCEPT given_head PROMPT 'Enter Expense Head: '
-ACCEPT start_date PROMPT 'Enter Start Date (YYYY-MM-DD): '
-ACCEPT end_date PROMPT 'Enter End Date (YYYY-MM-DD): '
+ACCEPT start_date PROMPT 'Enter Start Date (DD-MON-YY): '
+ACCEPT end_date PROMPT 'Enter End Date (DD-MON-YY): '
 
 SELECT ExpNumber, ExpDate, VendorName, Amount
 FROM ExpMast
-WHERE ExpDate BETWEEN TO_DATE('&start_date', 'YYYY-MM-DD') AND TO_DATE('&end_date', 'YYYY-MM-DD')
+WHERE ExpDate BETWEEN '&start_date' AND '&end_date'
 AND Head = '&given_head';
