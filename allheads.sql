b1c77aeb89d258d309cc310ccf8062af45ca0f0d
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-30 12:38:41 -0400

    Update allheads.sql

--- allheads.sql
+++ allheads.sql
@@ -1,3 +1,9 @@
+-- Name: Anthony Nelson
+-- Major: Computer Science
+-- Course: CSC 356 - Intro To Database Management Systems
+-- Semester & Year: Spring 2024
+-- Instructor: Dr. Hussain
+-- Assignment #: Project Phase II
 -- File: allheads.sql
 
 -- not sure yet
