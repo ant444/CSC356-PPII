cf1f696f5d305a77cbd03eb9c91c14d253205f19
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-30 12:41:39 -0400

    Update getexphead.sql

--- getexphead.sql
+++ getexphead.sql
@@ -1,4 +1,10 @@
--- File: getexphead.sql
+-- Name: Anthony Nelson
+-- Major: Computer Science
+-- Course: CSC 356 - Intro To Database Management Systems
+-- Semester & Year: Spring 2024
+-- Instructor: Dr. Hussain
+-- Assignment #: Project Phase II
+-- Filename: getexphead.sql
 
 ACCEPT given_head PROMPT 'Enter Expense Head: '
 ACCEPT start_date PROMPT 'Enter Start Date (DD-MON-YY): '
