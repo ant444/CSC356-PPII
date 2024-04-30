73401639c592c041b6f56d4e8e11eac75a1836bb
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-30 12:39:25 -0400

    Update createdb.sql

--- createdb.sql
+++ createdb.sql
@@ -1,4 +1,10 @@
--- createdb.sql
+-- Name: Anthony Nelson
+-- Major: Computer Science
+-- Course: CSC 356 - Intro To Database Management Systems
+-- Semester & Year: Spring 2024
+-- Instructor: Dr. Hussain
+-- Assignment #: Project Phase II
+-- Filename: createdb.sql
 
 -- Run deldb.sql to delete the existing database
 @deldb.sql
