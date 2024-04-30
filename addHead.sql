4fb6735e9a6a5a29b31e1fa697c16d922a5cf0a0
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-30 12:37:42 -0400

    Update addHead.sql

--- addHead.sql
+++ addHead.sql
@@ -1,6 +1,11 @@
+-- Name: Anthony Nelson
+-- Major: Computer Science
+-- Course: CSC 356 - Intro To Database Management Systems
+-- Semester & Year: Spring 2024
+-- Instructor: Dr. Hussain
+-- Assignment #: Project Phase II
 -- Filename: addHead.sql
 
--- addHead.sql
 
 -- Inserting data into the HEAD table
 INSERT INTO HEAD (Code, Name, HType, OpBal, HDate)
