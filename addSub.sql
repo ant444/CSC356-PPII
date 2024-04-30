8d76c35927919ebcd79e6228617c5e012d40604e
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-30 12:38:12 -0400

    Update addSub.sql

--- addSub.sql
+++ addSub.sql
@@ -1,3 +1,9 @@
+-- Name: Anthony Nelson
+-- Major: Computer Science
+-- Course: CSC 356 - Intro To Database Management Systems
+-- Semester & Year: Spring 2024
+-- Instructor: Dr. Hussain
+-- Assignment #: Project Phase II
 -- addSub.sql
 
 -- Inserting data into the SUBHEAD table
