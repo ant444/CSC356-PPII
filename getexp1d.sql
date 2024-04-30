7269da38fb57b737a5919f4f5c00dc926ed1a926
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-29 21:30:14 -0400

    Update getexp1d.sql

--- getexp1d.sql
+++ getexp1d.sql
@@ -1,25 +1,25 @@
 -- File: getexp1d.sql
- 1 ACCEPT given_date PROMPT 'Enter Date (YYYY-MM-DD): '
- 2 
- 3 -- Display all expenses (without details) incurred on the given date and calculate total expense
- 4 SELECT ExpNum, ExpDate, CashAmt AS TotalAmount
- 5 FROM ExpMast
- 6 WHERE ExpDate = TO_DATE('&given_date', 'YYYY-MM-DD')
- 7 UNION ALL
- 8 SELECT ExpByCC.ExpNum, ExpMast.ExpDate, Amt AS TotalAmount
- 9 FROM ExpByCC
-10 JOIN ExpMast ON ExpByCC.ExpNum = ExpMast.ExpNum
-11 WHERE ExpMast.ExpDate = TO_DATE('&given_date', 'YYYY-MM-DD')
-12 UNION ALL
-13 SELECT NULL, NULL, SUM(TotalAmount) AS TotalAmount
-14 FROM (
-15     SELECT CashAmt AS TotalAmount
-16     FROM ExpMast
-17     WHERE ExpDate = TO_DATE('&given_date', 'YYYY-MM-DD')
-18     UNION ALL
-19     SELECT Amt AS TotalAmount
-20     FROM ExpByCC
-21     JOIN ExpMast ON ExpByCC.ExpNum = ExpMast.ExpNum
-22     WHERE ExpMast.ExpDate = TO_DATE('&given_date', 'YYYY-MM-DD')
-23 );
-24 
+ ACCEPT given_date PROMPT 'Enter Date (YYYY-MM-DD): '
+ 
+ -- Display all expenses (without details) incurred on the given date and calculate total expense
+ SELECT ExpNum, ExpDate, CashAmt AS TotalAmount
+ FROM ExpMast
+ WHERE ExpDate = TO_DATE('&given_date', 'YYYY-MM-DD')
+ UNION ALL
+ SELECT ExpByCC.ExpNum, ExpMast.ExpDate, Amt AS TotalAmount
+ FROM ExpByCC
+ JOIN ExpMast ON ExpByCC.ExpNum = ExpMast.ExpNum
+ WHERE ExpMast.ExpDate = TO_DATE('&given_date', 'YYYY-MM-DD')
+ UNION ALL
+ SELECT NULL, NULL, SUM(TotalAmount) AS TotalAmount
+ FROM (
+     SELECT CashAmt AS TotalAmount
+     FROM ExpMast
+     WHERE ExpDate = TO_DATE('&given_date', 'YYYY-MM-DD')
+     UNION ALL
+     SELECT Amt AS TotalAmount
+     FROM ExpByCC
+     JOIN ExpMast ON ExpByCC.ExpNum = ExpMast.ExpNum
+     WHERE ExpMast.ExpDate = TO_DATE('&given_date', 'YYYY-MM-DD')
+ );
+ 
