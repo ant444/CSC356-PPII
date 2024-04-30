b697983b4d8addde8ab7fa599f876957fd0d4ab6
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-30 13:07:46 -0400

    Update getexp2d.sql

--- getexp2d.sql
+++ getexp2d.sql
@@ -6,22 +6,29 @@
 -- Assignment #: Project Phase II
 -- Filename: getexp2d.sql
 
- ACCEPT given_date PROMPT 'Enter Date (DD-MON-YY): '
-   
- COLUMN ExpNum FORMAT 999999
- COLUMN ExpDate FORMAT A10
- COLUMN Descrip FORMAT A30
- COLUMN TotalAmount FORMAT $999,999.99
- 
- -- Display all expenses (with details) incurred on the given date and calculate t>
- SELECT ExpDet.ExpNum, ExpMast.ExpDate, ExpDet.Descrip, ExpDet.Amt AS TotalAmount
- FROM ExpDet
- JOIN ExpMast ON ExpDet.ExpNum = ExpMast.ExpNum
- WHERE ExpMast.ExpDate = '&given_date'
- UNION ALL
- SELECT NULL, NULL, 'Total Amount:', SUM(Amt) AS TotalAmount
- FROM ExpDet
- JOIN ExpMast ON ExpDet.ExpNum = ExpMast.ExpNum
- WHERE ExpMast.ExpDate = '&given_date'
- 
+-- File: getexp2d.sql
+PROMPT The following script displays all expenses on a given date:
+ACCEPT given_date PROMPT 'Enter Expense Date (DD-MMM-YY): '
+
+SET VERIFY OFF
+COLUMN ExpNum FORMAT 999999
+COLUMN ExpDate FORMAT A10
+COLUMN Descrip FORMAT A30
+COLUMN ExpenseAmount FORMAT $999,999.99
+
+PROMPT The following expenses occurred on &given_date
+
+-- Display all expenses (with details) incurred on the given date and calculate total expense
+SELECT ExpDet.ExpNum, ExpMast.ExpDate, ExpDet.Descrip, ExpDet.Amt AS TotalAmount
+FROM ExpDet
+JOIN ExpMast ON ExpDet.ExpNum = ExpMast.ExpNum
+WHERE ExpMast.ExpDate = &given_date
+UNION ALL
+SELECT NULL, NULL, 'Total Amount:', SUM(Amt) AS TotalAmount
+FROM ExpDet
+JOIN ExpMast ON ExpDet.ExpNum = ExpMast.ExpNum
+WHERE ExpMast.ExpDate = &given_date;
+
+PROMPT PL/SQL procedure successfully completed.
+SET VERIFY ON
 
