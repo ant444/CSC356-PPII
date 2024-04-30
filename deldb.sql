6974160a1bc052f63a8b21cd1cfa99ef26ef9187
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-30 12:39:49 -0400

    Update deldb.sql

--- deldb.sql
+++ deldb.sql
@@ -1,4 +1,10 @@
--- deldb.sql
+-- Name: Anthony Nelson
+-- Major: Computer Science
+-- Course: CSC 356 - Intro To Database Management Systems
+-- Semester & Year: Spring 2024
+-- Instructor: Dr. Hussain
+-- Assignment #: Project Phase II
+-- Filename: deldb.sql
 
 -- Disable foreign key constraints
 SET CONSTRAINTS ALL DEFERRED;
