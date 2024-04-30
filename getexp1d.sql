941c37bb963e692a7689d28ffa9079fbe735987e
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-29 21:10:17 -0400

    Update getexp1d.sql

--- getexp1d.sql
+++ getexp1d.sql
@@ -1,11 +1,25 @@
 -- File: getexp1d.sql
-
-ACCEPT given_date PROMPT 'Enter Date (DD-MON-YY): '
-
-SELECT ExpNumber, ExpDate, StoreCode, CashAmt
-FROM ExpMast
-WHERE ExpDate = '&given_date';
-
-SELECT SUM(CashAmt) AS TotalAmount
-FROM ExpMast
-WHERE ExpDate = '&given_date' ; 
+ 1 ACCEPT given_date PROMPT 'Enter Date (YYYY-MM-DD): '
+ 2 
+ 3 -- Display all expenses (without details) incurred on the given date and calculate total expense
+ 4 SELECT ExpNum, ExpDate, CashAmt AS TotalAmount
+ 5 FROM ExpMast
+ 6 WHERE ExpDate = TO_DATE('&given_date', 'YYYY-MM-DD')
+ 7 UNION ALL
+ 8 SELECT ExpByCC.ExpNum, ExpMast.ExpDate, Amt AS TotalAmount
+ 9 FROM ExpByCC
+10 JOIN ExpMast ON ExpByCC.ExpNum = ExpMast.ExpNum
+11 WHERE ExpMast.ExpDate = TO_DATE('&given_date', 'YYYY-MM-DD')
+12 UNION ALL
+13 SELECT NULL, NULL, SUM(TotalAmount) AS TotalAmount
+14 FROM (
+15     SELECT CashAmt AS TotalAmount
+16     FROM ExpMast
+17     WHERE ExpDate = TO_DATE('&given_date', 'YYYY-MM-DD')
+18     UNION ALL
+19     SELECT Amt AS TotalAmount
+20     FROM ExpByCC
+21     JOIN ExpMast ON ExpByCC.ExpNum = ExpMast.ExpNum
+22     WHERE ExpMast.ExpDate = TO_DATE('&given_date', 'YYYY-MM-DD')
+23 );
+24 
